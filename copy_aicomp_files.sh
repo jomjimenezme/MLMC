dcddf498afbe6cdf296e397b03d3ef98723a4cfd
Jose Miguel Jimenez Merchan <jimenezmerchan1@jwlogin09.juwels>
2023-02-16 00:11:26 +0100

    submmit.job +.ini for 64x32to4 added

---
+++ copy_aicomp_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp aicomp_files/main.c src/
+cp aicomp_files/run .
+cp aicomp_files/Makefile .
+
---
+++ copy_juwels_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp juwels_files/main.c src/
+cp juwels_files/run .
+cp juwels_files/Makefile .
+
