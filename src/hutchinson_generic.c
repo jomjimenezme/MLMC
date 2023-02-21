c03675ed64594641c11c0bf9c979cd41e9c56bce
Jose Jimenez <jomjimenezme@gmail.com>
2023-02-21 17:20:43 +0100

    Corrected allocation ofhutchinson  memory wrt nr_iters + time prints

--- src/hutchinson_generic.c
+++ src/hutchinson_generic.c
@@ -365,7 +365,7 @@
       compute_core_start_end( 0, l->inner_vector_size, &start, &end, l, threading );
       vector_double_minus( h->mlmc_b1, p->x, h->mlmc_b2, start, end, l); 
 
-      if(l->depth ==0 && 1==1){
+      if(l->depth ==0 && 1==0){
         hutchinson_deflate_vector_double(h->rademacher_vector, l, threading);
       }
       return global_inner_product_double( h->rademacher_vector, h->mlmc_b1, p->v_start, p->v_end, l, threading );   
